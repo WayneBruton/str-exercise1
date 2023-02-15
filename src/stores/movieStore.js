62283bb8fdde630344a84ef4a17e7aece2d8d39e
Wayne Bruton <waynebruton@icloud.com>
2023-02-15 11:23:48 +0200

    changed movie url to https

--- src/stores/movieStore.js
+++ src/stores/movieStore.js
@@ -3,7 +3,7 @@ import { defineStore } from 'pinia';
 export const useMovieStore = defineStore('counter', {
   state: () => ({
     movieAPI:"836c2e67", // THIS WOULD ORDINARILY BE A SECRET
-    movieURL: "http://www.omdbapi.com/",
+    movieURL: "https://www.omdbapi.com/",
     movies:[
 
     ]
